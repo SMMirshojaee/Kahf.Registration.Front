427c662787530272c6bd89997bef6ed7032beaba
SeyyedMohammadMirshojaee <sm.mirshojaee@yahoo.com>
2025-07-06 22:29:09 +0330

    add iransans font

---
+++ src/app/share/imports.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ButtonModule } from "primeng/button";
+import { CheckboxModule } from "primeng/checkbox";
+import { RadioButtonModule } from "primeng/radiobutton";
+
+export const IMPORTS = [
+    ButtonModule,
+    CheckboxModule,
+    RadioButtonModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+]
--- src/app/welcome/welcome.ts
+++ src/app/welcome/welcome.ts
@@ -1,16 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
-import { ButtonModule } from 'primeng/button'; // add this line
+import { IMPORTS } from '@app/share/imports';
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [RouterModule,ButtonModule],
+  imports: [RouterModule, IMPORTS],
   templateUrl: './welcome.html',
   styleUrl: './welcome.scss'
 })
 export class Welcome {
+  ingredient: boolean = true;
   private router = inject(Router);
-  route(address:string) {
+  route(address: string) {
     this.router.navigate([address])
   }
 }
