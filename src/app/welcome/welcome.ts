1864349e26464ef0bd3ecce2ba96ddf8453616a4
SeyyedMohammadMirshojaee <sm.mirshojaee@yahoo.com>
2025-07-06 21:08:52 +0330

    before primeNg

--- src/app/welcome/welcome.ts
+++ src/app/welcome/welcome.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './welcome.html',
   styleUrl: './welcome.scss'
 })
 export class Welcome {
-
+  private router = inject(Router);
+  route(address:string) {
+    this.router.navigate([address])
+  }
 }
