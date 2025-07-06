fd91be9a0a9dce9db00024771332cc7453db40e6
SeyyedMohammadMirshojaee <sm.mirshojaee@yahoo.com>
2025-07-06 16:29:21 +0330

    ولکام و نات فوند

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadComponent: () => import('@app/welcome/welcome').then(c => c.Welcome) },
+    { path: '**', loadComponent: () => import('@app/not-found-page/not-found-page').then(c => c.NotFoundPage) }
+];
---
+++ src/app/welcome/welcome.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  standalone: true,
+  imports: [],
+  templateUrl: './welcome.html',
+  styleUrl: './welcome.scss'
+})
+export class Welcome {
+
+}
